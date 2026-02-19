92e30e395fee71bb9e51eb6161ccd9a1894cf684
DaniyarIssengaliyev <youiseni@gmail.com>
2026-02-20 00:13:13 +0500

    commit

--- lab3/task2/script.js
+++ lab3/task2/script.js
@@ -1,13 +1,16 @@
 const form = document.getElementById('todo-form');
 const input = document.getElementById('todo-input');
 const list = document.getElementById('todo-list');
-
+const btnShow = document.getElementById("hide");
 const trashIconSvg = `
 <svg class="trash" viewBox="0 0 24 24" aria-hidden="true">
   <path d="M9 3h6l1 2h4v2H4V5h4l1-2zm1 7h2v9h-2v-9zm4 0h2v9h-2v-9zM7 10h2v9H7v-9zm-1-1h12l-1 13a2 2 0 0 1-2 2H9a2 2 0 0 1-2-2L6 9z"/>
 </svg>
 `;
 
+btnShow.addEventListener('click', function(event){
+  list.classList.toggle('hide');
+})
 function createTodoItem(text) {
   const li = document.createElement('li');
   li.className = 'item';
